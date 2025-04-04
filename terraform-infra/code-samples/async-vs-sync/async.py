47fdd256592f670c3c846cb83d30418b3d7aff22
Philip Lee <philiplee@MacBookPro.lan>
2025-04-04 12:26:08 -0700

    up

--- terraform-infra/code-samples/async-vs-sync/async.py
+++ terraform-infra/code-samples/async-vs-sync/async.py
@@ -15,21 +15,15 @@ async def main():
 async def read_file_async():
     print("downloading file async")
     t1 = time.perf_counter(), time.process_time()
+    source_bucket = "lambda-source-test-bucket"
+    destination_bucket = "lambda-new-bucket"
+    key = "random_5gb_file"
 
     tracemalloc.start()
     session = aioboto3.Session()
-    async with session.resource("s3") as s3:
-        await s3.meta.client.download_file(
-            "lambda-source-test-bucket",
-            "random_5gb_file",
-            "/tmp/5gb-from-s3-bucket-async",
-        )
-
-        await s3.meta.client.upload_file(
-            "/tmp/5gb-from-s3-bucket-async",
-            "lambda-new-bucket",
-            "5gb-from-test-bucket-async",
-        )
+    copy_source = {"Bucket": source_bucket, "Key": key}
+    async with session.client("s3") as s3:
+        await s3.copy(CopySource=copy_source, Bucket=destination_bucket, Key=key)
     t2 = time.perf_counter(), time.process_time()
     print("Total time for async request was")
     print(f" Real time: {t2[0] - t1[0]:.2f} seconds")
